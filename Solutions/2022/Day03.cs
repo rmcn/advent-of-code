c715d5a4464c7c07503d26a16ab94c86d1c4d552
Ross McNab <rmcnab@gmail.com>
2023-10-19 22:26:09 +0100

    2022 Day 3

---
+++ Solutions/2022/Day03.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode.Year2022;
+
+class Day03 : IDay
+{
+    public int Year => 2022;
+    public int Day => 3;
+
+    public string One(string input) 
+    {
+        var t = 0;
+
+        foreach (var line in  input.Lines().Where(IsNotBlank))
+        {
+            var a = line.Substring(0, line.Length / 2);
+            var b = line.Substring(line.Length / 2, line.Length / 2);
+
+            var e = a.Except(a.Except(b.ToCharArray())).ToList().Single();
+
+            if (e.ToString().ToLower() == e.ToString())
+            {
+                t += e - 'a' + 1;
+            }
+            else
+            {
+                t += e - 'A' + 27;
+            }
+        }
+
+        return t.ToString();
+    }
+
+    public string Two(string input)
+    {
+        var t = 0;
+
+        foreach (var batch in input.Lines().Where(IsNotBlank).Batch(3))
+        {
+            var a = batch.ToList()[0];
+            var b = batch.ToList()[1];
+            var c = batch.ToList()[2];
+
+            var ab = a.Except(a.Except(b.ToCharArray())).ToList();
+            var e = ab.Except(ab.Except(c.ToCharArray())).Single();
+
+            if (e.ToString().ToLower() == e.ToString())
+            {
+                t += e - 'a' + 1;
+            }
+            else
+            {
+                t += e - 'A' + 27;
+
+            }
+        }
+
+        return t.ToString();
+    }
+}
