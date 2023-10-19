e88c07215dc8247bd91ae692f07dc1386f49a424
Ross McNab <rmcnab@gmail.com>
2023-10-19 22:34:17 +0100

    2022 Day 4

---
+++ Solutions/2022/Day04.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2022;
+
+class Day04 : IDay
+{
+    public int Year => 2022;
+    public int Day => 4;
+
+    public string One(string input) 
+    {
+        var t = 0;
+
+        foreach (var line in input.Lines().Where(l => l != ""))
+        {
+            var nums = new Regex("[0-9]+").Matches(line).Select(m => m.Value).Select(n => int.Parse(n)).ToList();
+            if ((nums[0] >= nums[2] && nums[1] <= nums[3]) || (nums[2] >= nums[0] && nums[3] <= nums[1]))
+            {
+                t++;
+            }
+        }
+
+        return t.ToString();
+    }
+
+    public string Two(string input)
+    {
+        var t = 0;
+
+        foreach (var line in input.Lines().Where(l => l != ""))
+        {
+            var nums = new Regex("[0-9]+").Matches(line).Select(m => m.Value).Select(n => int.Parse(n)).ToList();
+            if ((nums[0] < nums[2] && nums[1] < nums[2]) || (nums[0] > nums[3] && nums[1] > nums[3]))
+            {
+            }
+            else
+                t++;
+        }
+
+        return t.ToString();
+    }
+}
