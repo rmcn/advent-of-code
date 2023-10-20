2601e9bdb4c13e1480e10975ea6f9d6b39181c5e
Ross McNab <rmcnab@gmail.com>
2023-10-20 15:11:30 +0100

    2022 Day 6

---
+++ Solutions/2022/Day06.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode.Year2022;
+
+class Day06 : IDay
+{
+    public int Year => 2022;
+    public int Day => 6;
+
+    public (bool, string) One(string input) 
+    {
+        int i;
+        for (i = 0; i < input.Length; i++)
+        {
+            if (input.Skip(i).Take(4).Distinct().Count() == 4)
+            {
+                break;
+            }
+        }
+
+        return (true, (i + 4).ToString());
+    }
+
+    public (bool, string) Two(string input)
+    {
+        int i;
+        for (i = 0; i < input.Length; i++)
+        {
+            if (input.Skip(i).Take(14).Distinct().Count() == 14)
+            {
+                break;
+            }
+        }
+
+        return (true, (i + 14).ToString());
+    }
+}
