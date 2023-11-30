74ece010e7bd6c87735b8e8dfa8e82bda944f402
Ross McNab <rmcnab@gmail.com>
2023-11-30 19:24:47 +0000

    Starter template

---
+++ Solutions/TemplateForDay00.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Year0000;
+
+public class TemplateForDay00 : IDay
+{
+    public (bool, string) One(string input)
+    {
+        int t = 0;
+
+        var k = input.Lines().Where(IsNotBlank).Select((s, i) => s);
+
+        foreach(var line in input.Lines().Where(IsNotBlank))
+        {
+        }
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var c = input[i];
+        }
+
+
+        return (false, t.ToString());
+    }
+
+    public (bool, string) Two(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
