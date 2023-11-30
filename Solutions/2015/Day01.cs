6a01569cb154ffcb152acae1f8a185ad24ad387a
Ross McNab <rmcnab@gmail.com>
2023-11-30 19:12:54 +0000

    2015 day 1

---
+++ Solutions/2015/Day01.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Year2015;
+
+public class Day01: IDay
+{
+    public (bool, string) One(string input)
+    {
+        int t = 0;
+
+        var up = input.Trim().Select(c => c).Where(c => c == '(').Count();
+        var down = input.Trim().Select(c => c).Where(c => c == ')').Count();
+
+        return (true, (up - down).ToString());
+    }
+
+    public (bool, string) Two(string input)
+    {
+        int t = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var c = input[i];
+            if (c == '(') t++;
+            if (c == ')') t--;
+
+            if (t == -1)
+                return (true, (i + 1).ToString());
+        }
+
+        return (false, t.ToString());
+    }
+}
