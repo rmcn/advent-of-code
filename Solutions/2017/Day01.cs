23789746fa6fe9f728fad043d0a710d7dd32d750
Ross McNab <rmcnab@gmail.com>
2023-11-30 20:53:35 +0000

    2017 day 1

---
+++ Solutions/2017/Day01.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Year2017;
+
+public class Day01 : IDay
+{
+    public (bool, string) One(string input)
+    {
+        int t = 0;
+        var s = input.Trim();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var n = (i + 1) % s.Length;
+
+            if (s[i] == s[n])
+                t += s[i].ToString().Int();
+        }
+
+        return (true, t.ToString());
+    }
+
+    public (bool, string) Two(string input)
+    {
+        int t = 0;
+        var s = input.Trim();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var n = (i + s.Length/2) % s.Length;
+
+            if (s[i] == s[n])
+                t += s[i].ToString().Int();
+        }
+
+        return (true, t.ToString());
+    }
+}
