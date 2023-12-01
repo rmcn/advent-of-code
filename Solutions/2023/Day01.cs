34c4923abed5437626cdc0d96644a08658409d4f
Ross McNab <rmcnab@gmail.com>
2023-12-01 16:56:15 +0000

    2023 day 1

---
+++ Solutions/2023/Day01.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode.Year2023;
+
+using DigitMap = Dictionary<string, char>;
+
+public class Day01 : IDay
+{
+    public (bool, string) One(string input) =>
+    (
+        false,
+        input.Lines().Where(IsNotBlank).Select(CalibrationOne).Sum().ToString()
+    );
+
+    int CalibrationOne(string s) => $"{FirstDigit(s)}{LastDigit(s)}".Int();
+
+    public (bool, string) Two(string input) =>
+    (
+        false,
+        input.Lines().Where(IsNotBlank).Select(CalibrationTwo).Sum().ToString()
+    );
+
+    int CalibrationTwo(string s) => $"{FirstDigitOrWord(s)}{LastDigitOrWord(s)}".Int();
+
+    static DigitMap Digits = "123456789".ToDictionary(c => $"{c}", c => c);
+
+    static DigitMap Words = new()
+    {
+        {"one", '1'},
+        {"two", '2'},
+        {"three", '3'},
+        {"four", '4'},
+        {"five", '5'},
+        {"six", '6'},
+        {"seven", '7'},
+        {"eight", '8'},
+        {"nine", '9'}
+    };
+
+    static DigitMap DigitsAndWords = Merge(Digits, Words);
+
+    char FirstDigit(string s) => FirstMatch(s, Digits);
+    char LastDigit(string s) => LastMatch(s, Digits);
+    char FirstDigitOrWord(string s) => FirstMatch(s, DigitsAndWords);
+    char LastDigitOrWord(string s) => LastMatch(s, DigitsAndWords);
+
+    char FirstMatch(string s, DigitMap map)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            var substring = s[i..];
+
+            foreach (var key in map.Keys)
+                if (substring.StartsWith(key))
+                    return map[key];
+        }
+        throw new Exception("No match");
+    }
+
+    char LastMatch(string s, DigitMap map) => FirstMatch(Reverse(s), map.ToDictionary(p => Reverse(p.Key), p => p.Value));
+
+    private static string Reverse(string line)
+    {
+        return new string(line.Reverse().ToArray());
+    }
+}
--- Solutions/Globals.cs
+++ Solutions/Globals.cs
@@ -9,5 +9,17 @@ namespace AdventOfCode
         public static bool IsBlank(string s) => string.IsNullOrWhiteSpace(s);
         public static bool IsNotBlank(string s) => !IsBlank(s);
         public static int Int(string s) => int.Parse(s);
+
+        public static Dictionary<TKey, TValue> Merge<TKey, TValue>(params IDictionary<TKey, TValue>[] dicts)
+            where TKey: notnull
+        {
+            var result = new Dictionary<TKey, TValue>();
+
+            foreach (var d in dicts)
+                foreach (var kvp in d)
+                    result[kvp.Key] = kvp.Value;
+
+            return result;
+        }
     }
 }
