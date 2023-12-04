8f75a58ae5612baeb3c2c830d743326431ec0d39
Ross McNab <rmcnab@gmail.com>
2023-12-04 08:31:07 +0000

    2023 day 4

---
+++ Solutions/2023/Day04.cs
@@ -0,0 +1,63 @@
+﻿using System.Data.Common;
+using MoreLinq.Extensions;
+
+namespace AdventOfCode.Year2023;
+
+public class Day04 : Solution
+{
+    public override string Example => @"";
+
+    public override object One(string input)
+    {
+        var cards = input.Lines().Where(IsNotBlank).Select(c => new Card(c)).ToList();
+        return cards.Sum(c => (int)Pow(2, c.MatchCount - 1));
+    }
+
+    public override object Two(string input)
+    {
+        var cards = input.Lines().Where(IsNotBlank).Select(c => new Card(c)).ToList();
+
+        var t = cards.Count;
+
+        var candidates = new Stack<Card>(cards);
+
+        int c = 0;
+
+        while (candidates.Count > 0)
+        {
+            var card = candidates.Pop();
+
+            var isWin = card.MatchCount > 0;
+
+            if (isWin)
+            {
+                t += card.MatchCount;
+
+                for (int i = 0; i < card.MatchCount; i++)
+                {
+                    candidates.Push(cards[card.Index + i]);
+                }
+            }
+        }
+
+        return t;
+    }
+
+    class Card
+    {
+        public Card(string card)
+        {
+            var game = card.Split(':');
+
+            Index = game[0].Int();
+
+            var nums = game[1].Split('|');
+            var win = new HashSet<int>(nums[0].Ints());
+            var got = new HashSet<int>(nums[1].Ints());
+            MatchCount = win.Intersect(got).Count();
+        }
+
+        public int Index { get; set; }
+        public int MatchCount { get; set; }
+    }
+}
