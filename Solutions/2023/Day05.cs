8719aab6ef5f220fda3af62cd6a1c4cc31fee3a9
Ross McNab <rmcnab@gmail.com>
2023-12-05 13:24:05 +0000

    Refactor 2023 day 5

--- Solutions/2023/Day05.cs
+++ Solutions/2023/Day05.cs
@@ -5,28 +5,17 @@ namespace AdventOfCode.Year2023;
 
 public class Day05 : Solution
 {
-    public override string Example => @"";
-
     public override Answer One(string input)
     {
-        var seeds = input.Lines().First().Longs();
-
         var maps = input.Lines().Skip(1).Where(IsNotBlank)
             .Segment(l => l.Contains(':'))
             .Select(ParseMap)
             .ToList();
 
+        var seedRanges = input.Lines().First().Longs()
+            .Select(s => new ValueRange(s, 1)).ToList();
 
-        var ranges = seeds.Select(s => new ValueRange(s, 1)).ToList();
-
-        return Chase(ranges, maps).Min(r => r.Start);
-    }
-
-    record Entry(long DestStart, long SourceStart, long Length);
-
-    private List<Entry> ParseMap(IEnumerable<string> entries)
-    {
-        return entries.Where(e => !e.Contains(':')).Select(e => e.Longs()).Select(v => new Entry(v[0], v[1], v[2])).ToList();
+        return ApplyMaps(seedRanges, maps).Min(r => r.Start);
     }
 
     public override Answer Two(string input)
@@ -36,68 +25,78 @@ public class Day05 : Solution
             .Select(ParseMap)
             .ToList();
 
-        var seedRanges = input.Lines().First().Longs().Batch(2)
+        var seedRanges = input.Lines().First().Longs()
+            .Batch(2)
             .Select(p => p.ToArray())
             .Select(p => new ValueRange(p[0], p[1]))
             .ToList();
 
-        return Chase(seedRanges, maps).Min(r => r.Start);
+        return ApplyMaps(seedRanges, maps).Min(r => r.Start);
     }
 
 
-    private List<ValueRange> Chase(List<ValueRange> ranges, List<List<Entry>> maps)
+    record MapEntry(long DestStart, long SourceStart, long Length);
+
+    private List<MapEntry> ParseMap(IEnumerable<string> entries) =>
+        entries
+            .Where(e => !e.Contains(':'))
+            .Select(e => e.Longs())
+            .Select(v => new MapEntry(v[0], v[1], v[2]))
+            .ToList();
+
+    private List<ValueRange> ApplyMaps(List<ValueRange> ranges, List<List<MapEntry>> maps)
     {
         foreach (var map in maps)
         {
-            Log($"Amount {ranges.Count}");
+            Log($"Range count {ranges.Count}");
             ranges = ApplyMap(ranges, map);
             LogEx(string.Join(", ", ranges));
         }
         return ranges;
     }
 
-    List<ValueRange> ApplyMap(List<ValueRange> ranges, List<Entry> map)
+    List<ValueRange> ApplyMap(List<ValueRange> ranges, List<MapEntry> map)
     {
         Stack<ValueRange> inputRanges = new(ranges);
-        List<ValueRange> outputRanges = new();
+        List<ValueRange> mappedRanges = new();
 
         while (inputRanges.Count > 0)
         {
             var range = inputRanges.Pop();
-            bool mapped = false;
+            bool isMatched = false;
             foreach (var entry in map)
             {
-                // Contained
-                if (range.Start >= entry.SourceStart && range.End() < entry.SourceStart + entry.Length)
+                // Input range contained within map entry - output single mapped range
+                if (range.Start >= entry.SourceStart && range.End < entry.SourceStart + entry.Length)
                 {
-                    outputRanges.Add(new ValueRange(range.Start - entry.SourceStart + entry.DestStart, range.Count));
-                    mapped = true;
+                    mappedRanges.Add(new ValueRange(range.Start - entry.SourceStart + entry.DestStart, range.Count));
+                    isMatched = true;
                     break;
                 }
-                // Start before, end within
-                else if (range.Start < entry.SourceStart && range.End() >= entry.SourceStart && range.End() < entry.SourceStart + entry.Length)
+                // Input range starts before and ends within map entry - split into mapped range and other input
+                else if (range.Start < entry.SourceStart && range.End >= entry.SourceStart && range.End < entry.SourceStart + entry.Length)
                 {
                     long countWithin = range.Start + range.Count - entry.SourceStart;
-                    outputRanges.Add(new ValueRange(entry.SourceStart - entry.SourceStart + entry.DestStart, countWithin));
+                    mappedRanges.Add(new ValueRange(entry.SourceStart - entry.SourceStart + entry.DestStart, countWithin));
                     inputRanges.Push(new ValueRange(range.Start, range.Count - countWithin));
-                    mapped = true;
+                    isMatched = true;
                     break;
                 }
-                // Start within, end after
-                else if (range.Start >= entry.SourceStart && range.Start < entry.SourceStart + entry.Length && range.End() >= entry.SourceStart + entry.Length)
+                // Input range starts within and ends after map entry - split into mapped range and other input
+                else if (range.Start >= entry.SourceStart && range.Start < entry.SourceStart + entry.Length && range.End >= entry.SourceStart + entry.Length)
                 {
                     long countWithin = entry.SourceStart + entry.Length - range.Start;
-                    outputRanges.Add(new ValueRange(range.Start - entry.SourceStart + entry.DestStart, countWithin));
+                    mappedRanges.Add(new ValueRange(range.Start - entry.SourceStart + entry.DestStart, countWithin));
                     inputRanges.Push(new ValueRange(entry.SourceStart + entry.Length, range.Count - countWithin));
-                    mapped = true;
+                    isMatched = true;
                     break;
                 }
             }
-            // Not mapped
-            if (!mapped)
-                outputRanges.Add(range);
+            // No map etries match - pass through unchanged
+            if (!isMatched)
+                mappedRanges.Add(range);
         }
-        return outputRanges;
+        return mappedRanges;
     }
 
 
@@ -110,8 +109,7 @@ public class Day05 : Solution
         }
         public long Start { get; set; }
         public long Count { get; set; }
-        public long End() => Start + Count - 1;
-        public bool Contains(long v) => v >= Start && v <= End();
+        public long End => Start + Count - 1;
         public override string ToString() => $"{Start}-{Count}";
     }
 }
