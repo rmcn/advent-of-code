36ffa17cade16f4ada288080c9aa4254e2858b87
Ross McNab <rmcnab@gmail.com>
2023-12-06 07:33:53 +0000

    2023 day 6

---
+++ Solutions/2023/Day06.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode.Year2023;
+
+public class Day06 : Solution
+{
+    public override Answer One(string input)
+    {
+        ulong t = 1;
+
+        var lines = input.Lines().Where(IsNotBlank).ToList();
+        var times = lines[0].Ints();
+        var dists = lines[1].Ints();
+
+        for (int i = 0; i < times.Count; i++)
+        {
+            var time = times[i];
+            var dist = dists[i];
+
+            ulong ways = 0;
+            for (int c = 0; c < time; c++)
+            {
+                var left = time - c;
+                var travel = left * c;
+                if (travel > dist)
+                    ways++;
+            }
+
+            t *= ways;
+        }
+
+        return t;
+    }
+
+    public override Answer Two(string input)
+    {
+        var lines = input.Lines().Where(IsNotBlank).ToList();
+        var time = lines[0].Replace(" ", "").Long();
+        var dist = lines[1].Replace(" ", "").Long();
+
+        ulong ways = 0;
+        for (int c = 0; c < time; c++)
+        {
+            var left = time - c;
+            var travel = left * c;
+            if (travel > dist)
+                ways++;
+        }
+        return ways;
+    }
+}
--- Solutions/Extensions.cs
+++ Solutions/Extensions.cs
@@ -7,6 +7,7 @@ public static class Extensions
 {
     public static string[] Lines(this string s) => s.Split('\n');
     public static int Int(this string s) => s.Ints().Single();
+    public static long Long(this string s) => s.Longs().Single();
     public static List<int> Ints(this string s) => new Regex("-?[0-9]+").Matches(s).Select(m => m.Value).Select(n => int.Parse(n)).ToList();
     public static List<long> Longs(this string s) => new Regex("-?[0-9]+").Matches(s).Select(m => m.Value).Select(n => long.Parse(n)).ToList();
     public static List<int> PosInts(this string s) => new Regex("[0-9]+").Matches(s).Select(m => m.Value).Select(n => int.Parse(n)).ToList();
