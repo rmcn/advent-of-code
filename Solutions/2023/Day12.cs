e930637297b3d8f83896dc153bcc12bdf0bc02ec
Ross McNab <rmcnab@gmail.com>
2023-12-12 19:46:10 +0000

    Skip ahead

--- Solutions/2023/Day12.cs
+++ Solutions/2023/Day12.cs
@@ -12,13 +12,13 @@ public class Day12 : Solution
     Puzzle ParsePuzzle(string s)
     {
         var parts = s.Split(' ');
-        return new Puzzle(parts[0].Trim(), parts[1].Ints().ToArray());
+        return new Puzzle(parts[0].Trim() + '.', parts[1].Ints().ToArray());
     }
 
     Puzzle ParsePuzzleTwo(string s)
     {
         var parts = s.Split(' ');
-        return new Puzzle(RepeatFive(parts[0].Trim(), '?'), RepeatFive(parts[1], ',').Ints().ToArray());
+        return new Puzzle(RepeatFive(parts[0].Trim(), '?') + '.', RepeatFive(parts[1], ',').Ints().ToArray());
     }
 
     string RepeatFive(string s, char c) => s + c + s + c + s + c + s + c + s;
@@ -33,7 +33,7 @@ public class Day12 : Solution
 
         foreach (var p in puzzles)
         {
-            var c = CountValid(p.Springs, p.Lengths, 0);
+            var c = CountValid(p.Springs, p.Lengths, 0, 0);
             LogEx($"{p.Springs} count {c}");
             t += c;
         }
@@ -49,6 +49,21 @@ public class Day12 : Solution
         chars[i] = r;
         return new string(chars);
     }
+
+    private string ReplaceRange(string s, int start, int brokenCount)
+    {
+        var chars = s.ToCharArray();
+        for (int i = start; i < start + brokenCount; i++)
+        {
+            chars[i] = '#';
+        }
+
+        if (start + brokenCount < s.Length)
+            chars[start + brokenCount] = '.';
+
+        return new string(chars);
+    }
+
 
 
     bool IsValid(string s, int[] lengths)
@@ -80,34 +95,76 @@ public class Day12 : Solution
         return true;*/
     }
 
-    private int CountValid(string springs, int[] lengths, int i)
+    private int CountValid(string springs, int[] lengths, int i, int b)
     {
         if (i == springs.Length)
             return IsValid(springs, lengths) ? 1 : 0;
 
-        if (springs[i] != '?')
-            return CountValid(springs, lengths, i + 1);
+        if (springs[i] == '.')
+            return CountValid(springs, lengths, i + 1, b);
 
-        if (!IsPotential(springs, lengths))
-            return 0;
+
+        if (springs[i] == '#')
+        {
+            if (b >= lengths.Length)
+                return 0;
+
+            if (CanPlace(springs, i, lengths[b]))
+            {
+                var withRange = ReplaceRange(springs, i, lengths[b]);
+                return CountValid(withRange, lengths, i + lengths[b] + 1, b + 1);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
 
         int t = 0;
-        t += CountValid(ReplaceAt(springs, i, '.'), lengths, i + 1);
-        t += CountValid(ReplaceAt(springs, i, '#'), lengths, i + 1);
+
+        if (b < lengths.Length && CanPlace(springs, i, lengths[b]))
+        {
+            var withRange = ReplaceRange(springs, i, lengths[b]);
+            t += CountValid(withRange, lengths, i + lengths[b] + 1, b + 1);
+        }
+
+        var withSpring = ReplaceAt(springs, i, '.');
+        t += CountValid(withSpring, lengths, i + 1, b);
+
         return t;
+    }
+
+
+    // Can place a sequence of broken springs and a final '.' (or end of list)
+    private bool CanPlace(string springs, int start, int brokenCount)
+    {
+        if (start + brokenCount > springs.Length)
+            return false;
+
+        for (int i = start; i < start + brokenCount; i++)
+        {
+            if (springs[i] == '.')
+                return false;
+        }
+
+        if (start + brokenCount == springs.Length) // At end of string, no need for '.' terminator
+            return true;
+
+        // must end with a '.' or a '?'
+        return springs[start + brokenCount] != '#';
     }
 
     public override Answer Two(string input)
     {
-        return 0;
         int t = 0;
 
         var puzzles = input.Lines().Where(IsNotBlank).Select(ParsePuzzleTwo).ToList();
 
         foreach (var p in puzzles)
         {
-            var c = CountValid(p.Springs, p.Lengths, 0);
-            LogEx($"{p.Springs} count {c}");
+            Log($"Processing {p.Springs} {string.Join(',', p.Lengths)}");
+            var c = CountValid(p.Springs, p.Lengths, 0, 0);
             t += c;
         }
 
