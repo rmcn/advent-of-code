ec9b3c986c0cd6966a7fa8f5fe6d209ca67d0ec3
Ross McNab <rmcnab@gmail.com>
2023-12-18 21:11:14 +0000

    2023 day 18 part 2

--- Solutions/2023/Day18.cs
+++ Solutions/2023/Day18.cs
@@ -4,27 +4,68 @@ namespace AdventOfCode.Year2023;
 
 public class Day18 : Solution
 {
-    public override string Example => @"";
+    /*
+        ###
+        # #
+        # ###
+        #   #
+        #   #
+        #####
+
+        #####
+        #   #
+        #   #
+        # ###
+        # #
+        ###
+
+            ###
+          ### #
+          #   #
+          ### #
+            # #
+          ### #
+          #   #
+          #####
+    */
+    public override string Example => @"R 2
+D 2
+R 2
+D 3
+L 4
+U 5";
+
+    record Step(char Dir, int Dist);
+
+    Step ParseOne(string s)
+    {
+        var p = s.Split(' ');
+        return new Step(p[0][0], p[1].Int());
+    }
 
     public override Answer One(string input)
     {
+        var steps = input
+            .Lines()
+            .Where(IsNotBlank)
+            .Select(ParseOne)
+            .ToList();
 
         var holes = new HashSet<Point>();
 
         var current = new Point(0, 0);
         holes.Add(current);
 
-        foreach (var line in input.Lines().Where(IsNotBlank))
+        foreach (var step in steps)
         {
-            var dir = line[0];
-            var dist = line.Split(' ')[1].Int();
-            if (dir == 'U')
+            var dist = step.Dist;
+            if (step.Dir == 'U')
                 while (dist-- != 0) { current.Y -= 1; holes.Add(current); }
-            else if (dir == 'D')
+            else if (step.Dir == 'D')
                 while (dist-- != 0) { current.Y += 1; holes.Add(current); }
-            else if (dir == 'L')
+            else if (step.Dir == 'L')
                 while (dist-- != 0) { current.X -= 1; holes.Add(current); }
-            else if (dir == 'R')
+            else if (step.Dir == 'R')
                 while (dist-- != 0) { current.X += 1; holes.Add(current); }
             else
                 throw new Exception();
@@ -50,8 +91,112 @@ public class Day18 : Solution
         return holes.Count;
     }
 
+    record Line(int X, int Y1, int Y2);
+
     public override Answer Two(string input)
     {
-        return 0;
+        var steps = input
+            .Lines()
+            .Where(IsNotBlank)
+            .Select(ParseOne)
+            .ToList();
+
+        var lines = new List<Line>();
+
+        var current = new Point(0, 0);
+        foreach (var step in steps)
+        {
+            var dist = step.Dist;
+            if (step.Dir == 'U') {
+                var prev = current;
+                while (dist-- != 0) { current.Y -= 1; }
+                lines.Add(new Line(current.X, Min(prev.Y, current.Y), Max(prev.Y, current.Y)));
+            } else if (step.Dir == 'D') {
+                var prev = current;
+                while (dist-- != 0) { current.Y += 1; }
+                lines.Add(new Line(current.X, Min(prev.Y, current.Y), Max(prev.Y, current.Y)));
+            } else if (step.Dir == 'L') {
+                while (dist-- != 0) { current.X -= 1; }
+            } else if (step.Dir == 'R') {
+                while (dist-- != 0) { current.X += 1; }
+            } else
+                throw new Exception();
+        }
+
+        lines = lines.OrderBy(l => l.X).ThenBy(l => l.Y1).ToList();
+
+        var xes = lines.Select(l => l.X).Distinct().ToList();
+
+        var front = lines.Where(l => l.X == xes[0]).ToList();
+
+        long total = 0;
+        total += front.Select(l => l.Y2 - l.Y1 + 1).Sum();
+        for (int i = 1; i < xes.Count; i++)
+        {
+            var xs = xes[i-1];
+            var xe = xes[i];
+            long width = xe - xs - 1;
+
+            total += front.Select(l => width * (l.Y2 - l.Y1 + 1)).Sum();
+
+            // Add or extend now, split or reduce after
+            var newLines = lines.Where(l => l.X == xe).ToList();
+            var reducedBy = 0;
+            foreach (var line in newLines)
+            {
+                var extend = front.Where(fl => fl.Y1 == line.Y2 || fl.Y2 == line.Y1).ToList();
+                var reduce = front.Where(fl => fl.Y1 == line.Y1 || fl.Y2 == line.Y2).ToList();
+                var split = front.Where(fl => fl.Y1 < line.Y1 && fl.Y2 > line.Y2).ToList();
+
+                if (extend.Count > 2) throw new Exception();
+                if (reduce.Count > 1) throw new Exception();
+                if (split.Count > 1) throw new Exception();
+
+                var match = (extend.Count > 0 ? 1 : 0) + (reduce.Count > 0 ? 1 : 0) + (split.Count > 0 ? 1 : 0);
+                if (match > 1) throw new Exception();
+
+                if (extend.Count > 0)
+                {
+                    front = front.Except(extend).ToList();
+                    extend.Add(line);
+                    front.Add(new Line(xe, extend.Min(l => l.Y1), extend.Max(l => l.Y2)));
+                }
+                else if (reduce.Count > 0)
+                {
+                    front = front.Except(reduce).ToList();
+                    if (reduce[0].Y1 == line.Y1 && reduce[0].Y2 == line.Y2)
+                    {
+                        // entirely gone
+                        reducedBy += line.Y2 - line.Y1 + 1;
+                    }
+                    else if (reduce[0].Y1 == line.Y1)
+                    {
+                        var shortened = new Line(xe, Min(line.Y2, reduce[0].Y2), Max(line.Y2, reduce[0].Y2));
+                        reducedBy += shortened.Y1 - line.Y1;
+                        front.Add(shortened);
+                    }
+                    else if (reduce[0].Y2 == line.Y2)
+                    {
+                        var shortened = new Line(xe, Min(line.Y1, reduce[0].Y1), Max(line.Y1, reduce[0].Y1));
+                        reducedBy += line.Y2 - shortened.Y2;
+                        front.Add(shortened);                        
+                    }
+                }
+                else if (split.Count > 0)
+                {
+                    front = front.Except(split).ToList();
+                    front.Add(new Line(xe, split[0].Y1, line.Y1));
+                    front.Add(new Line(xe, line.Y2, split[0].Y2));
+                    reducedBy = line.Y2 - line.Y1 - 1;
+                }
+                else
+                {
+                    front.Add(line);
+                }
+            }
+            total += front.Select(l => l.Y2 - l.Y1 + 1).Sum() + reducedBy;
+        }
+
+        return total;
     }
 }
