189652a8b6f0dc8f0b759969784fb6eed4551dd8
Ross McNab <rmcnab@gmail.com>
2023-12-21 22:00:27 +0000

    2023 day 21

---
+++ Solutions/2023/Day21.cs
@@ -0,0 +1,118 @@
+﻿using System.Drawing;
+using System.Net.Security;
+using System.Numerics;
+
+namespace AdventOfCode.Year2023;
+
+public class Day21 : Solution
+{
+    public override Answer One(string input)
+    {
+        var goalSteps = input.Length < 1000 ? 6 : 64;
+
+        var g = Grid.Parse(input, '#');
+
+        var frontier = new List<Point>
+        {
+            g.Cells.Where(c => c.Value == 'S').Select(c => c.Key).Single()
+        };
+
+        for (int step = 1; step <= goalSteps; step++)
+        {
+            frontier = frontier
+                .SelectMany(p => new[]
+                {
+                    p with { X = p.X - 1 },
+                    p with { X = p.X + 1 },
+                    p with { Y = p.Y - 1 },
+                    p with { Y = p.Y + 1 }
+                })
+                .Where(p => g[p] != '#')
+                .Distinct()
+                .ToList();
+        }
+
+        return frontier.Count;
+    }
+
+    const long PartTwoGoalSteps = 26501365;
+
+    public override Answer Two(string input)
+    {
+        if (input.Length < 1000) return 0; // Skip example input
+
+        var g = Grid.Parse(input, '#', infinite: true);
+
+        var frontier = new HashSet<Point>
+        {
+            g.Cells.Where(c => c.Value == 'S').Select(c => c.Key).Single()
+        };
+
+        var sequence = new List<int>() { 1 };
+
+        while (sequence.Count < 600)
+        {
+            frontier = Step(g, frontier);
+            sequence.Add(frontier.Count);
+            if (sequence.Count % 10 == 0)
+                Console.Write(".");
+        }
+        Console.WriteLine();
+
+        var (skip, every, a, b, c) = DeriveQuadratic(sequence);
+
+        var n = (PartTwoGoalSteps - skip) / every + 1;
+
+        return a * n * n + b * n + c;
+    }
+
+    private (int skip, int every, int a, int b, int c) DeriveQuadratic(List<int> counts)
+    {
+        for (int skip = 0; skip <= counts.Count; skip++)
+        {
+            for (int every = 1; every <= counts.Count; every++)
+            {
+                // Only want skip & every values that neatly divide into our goal steps
+                if ((PartTwoGoalSteps - skip) % every != 0)
+                    continue;
+
+                var values = counts.Skip(skip).TakeEvery(every).ToList();
+                if (values.Count < 5) // want at least 5 values to give us three second-differences for comparison
+                    continue;
+
+                var diffs = values.Zip(values.Skip(1)).Select(p => p.Second - p.First).ToList();
+                var diffdiffs = diffs.Zip(diffs.Skip(1)).Select(p => p.Second - p.First).ToList();
+
+                if (diffdiffs.Distinct().Count() == 1)
+                {
+                    Log($"Skip {skip} take every {every} has constant 2nd order diff of {diffdiffs.First()}, first 5 vals [{string.Join(", ", values.Take(5))}]");
+                    var a = diffdiffs.First() / 2;
+                    var b = diffs.First() - 3 * a;
+                    var c = values.First() - a - b;
+                    return (skip, every, a, b, c);
+                }
+            }
+        }
+        throw new Exception("No quadractic with integer coefficients found");
+    }
+
+    private static HashSet<Point> Step(Grid g, HashSet<Point> frontier)
+    {
+        var next = new HashSet<Point>();
+
+        foreach (var p in frontier)
+        {
+            var l = p with { X = p.X - 1 };
+            var r = p with { X = p.X + 1 };
+            var u = p with { Y = p.Y - 1 };
+            var d = p with { Y = p.Y + 1 };
+
+            if (g[l] != '#') next.Add(l);
+            if (g[r] != '#') next.Add(r);
+            if (g[u] != '#') next.Add(u);
+            if (g[d] != '#') next.Add(d);
+        }
+
+        return next;
+    }
+}
--- Solutions/Grid.cs
+++ Solutions/Grid.cs
@@ -1,27 +1,31 @@
 ﻿using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace AdventOfCode;
 
 public class Grid : Grid<char>
 {
-    private Grid(string input, char defaultValue) : base(input, c => c, defaultValue)
+    private Grid(string input, char defaultValue, bool infinite) : base(input, c => c, defaultValue, infinite)
     {
     }
 
-    public static Grid Parse(string input, char defaultValue) => new Grid(input, defaultValue);
-    public static Grid<T> Parse<T>(string input, Func<char, T> parse, T defaultValue) => new(input, parse, defaultValue);
+    public static Grid Parse(string input, char defaultValue, bool infinite = false)
+        => new Grid(input, defaultValue, infinite);
+    public static Grid<T> Parse<T>(string input, Func<char, T> parse, T defaultValue, bool infinite = false)
+        => new(input, parse, defaultValue, infinite);
 }
 
 public class Grid<T>
 {
     private readonly T _default;
+    private readonly bool _infinite;
 
     public Dictionary<Point, T> Cells { get; } = new();
 
     public int Width { get; }
     public int Height { get; }
 
-    public Grid(string input, Func<char, T> parse, T defaultValue)
+    public Grid(string input, Func<char, T> parse, T defaultValue, bool infinite)
     {
         var rows = input.Lines().Where(IsNotBlank).ToList();
 
@@ -29,11 +33,36 @@ public class Grid<T>
         Height = rows.Count;
 
         _default = defaultValue;
+        _infinite = infinite;
 
         foreach (var (s, y) in rows.Select((s, y) => (s, y)))
             foreach (var (c, x) in s.ToCharArray().Select((c, x) => (c, x)))
                 Cells[new Point(x, y)] = parse(c);
     }
 
-    public T this[Point p] => Cells.GetValueOrDefault(p, _default);
+    public T this[Point p]
+    {
+        get => _infinite ? Cells[Wrap(p)] : Cells.GetValueOrDefault(p, _default);
+        set
+        {
+            if (_infinite)
+                p = Wrap(p);
+            Cells[p] = value;
+        }
+    }
+
+    private Point Wrap(Point p)
+    {
+        var modX = p.X % Width;
+        var x = p.X < 0
+            ? Width + (modX == 0 ? -Width : modX)
+            : modX;
+
+        var modY = p.Y % Height;
+        var y = p.Y < 0
+            ? Height + (modY == 0 ? -Height : modY)
+            : modY;
+
+        return new Point(x, y);
+    }
 }
