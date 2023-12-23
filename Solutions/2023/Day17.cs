550e7a64c536fafe89c4c1497abec7a037f644cf
Ross McNab <rmcnab@gmail.com>
2023-12-23 11:48:51 +0000

    Refactor 2023 day 17 to share A* code between parts

--- Solutions/2023/Day17.cs
+++ Solutions/2023/Day17.cs
@@ -32,44 +32,32 @@ public class Day17 : Solution
     public State Right(State s) => Move(s, s.Dir == Face.West ? Face.North : s.Dir + 1, 1);
     public State Forward(State s) => Move(s, s.Dir, s.Steps + 1);
 
-    public override Answer One(string input)
+    public override Answer One(string input) => AStar(input, SuccessorsOne);
+
+    private List<State> SuccessorsOne(Grid<int> g, State current)
+        => new[] { Left(current), Right(current), Forward(current) }
+            .Where(s => s.Steps <= 3 && g[s.Loc] != -1)
+            .ToList();
+
+    public override Answer Two(string input) => AStar(input, SuccessorsTwo);
+
+    private List<State> SuccessorsTwo(Grid<int> g, State current)
     {
-        var g = Grid.Parse(input, c => int.Parse(c.ToString()), -1);
-
-        var frontier = new PriorityQueue<State, int>();
-        var start = new State(new Point(0, 0), Face.East, 0);
-        frontier.Enqueue(start, 0);
-
-        var costSoFar = new Dictionary<State, int>
+        var successors = new List<State>
         {
-            [start] = 0
+            Forward(current)
         };
 
-        while (frontier.Count > 0)
+        if (current.Steps >= 4)
         {
-            var current = frontier.Dequeue();
-
-            if (current.Loc.X == g.Width - 1 && current.Loc.Y == g.Height - 1)
-                return costSoFar[current];
-
-            var successors = new[] { Left(current), Right(current), Forward(current) }.Where(s => s.Steps <= 3 && g[s.Loc] != -1).ToList();
-
-            foreach (var next in successors)
-            {
-                var newCost = costSoFar[current] + g[next.Loc];
-
-                if (!costSoFar.ContainsKey(next) || newCost < costSoFar[next])
-                {
-                    costSoFar[next] = newCost;
-                    var priority = newCost + Abs(g.Width - 1 - next.Loc.X) + Abs(g.Height - 1 - next.Loc.Y);
-                    frontier.Enqueue(next, priority);
-                }
-            }
+            successors.Add(Left(current));
+            successors.Add(Right(current));
         }
-        return 0;   
+
+        return successors.Where(s => s.Steps <= 10 && g[s.Loc] != -1).ToList();
     }
 
-    public override Answer Two(string input)
+    public Answer AStar(string input, Func<Grid<int>, State, List<State>> successors)
     {
         var g = Grid.Parse(input, c => int.Parse(c.ToString()), -1);
 
@@ -89,20 +77,7 @@ public class Day17 : Solution
             if (current.Loc.X == g.Width - 1 && current.Loc.Y == g.Height - 1)
                 return costSoFar[current];
 
-            var successors = new List<State>
-            {
-                Forward(current)
-            };
-
-            if (current.Steps >= 4)
-            {
-                successors.Add(Left(current));
-                successors.Add(Right(current));
-            }
-            
-            successors = successors.Where(s => s.Steps <= 10 && g[s.Loc] != -1).ToList();
-
-            foreach (var next in successors)
+            foreach (var next in successors(g, current))
             {
                 var newCost = costSoFar[current] + g[next.Loc];
 
@@ -116,4 +91,6 @@ public class Day17 : Solution
         }
         return 0;
      }
+
+
 }
