11e3d5db54a236be5580dc42144af3da4122e52d
Ross McNab <rmcnab@gmail.com>
2023-12-25 19:59:07 +0000

    2023 day 14 part 1

--- Solutions/2023/Day24.cs
+++ Solutions/2023/Day24.cs
@@ -59,6 +59,31 @@ public class Day24 : Solution
         return new Hailstone(new V3(p[0], p[1], p[2]), new V3(v[0], v[1], v[2]));
     }
 
+    public record V3Dec(decimal X, decimal Y, decimal Z);
+
+    public class HailstoneTwo
+    {
+        public HailstoneTwo(V3Dec p, V3Dec v)
+        {
+            P = p;
+            V = v;
+        }
+
+        public V3Dec P { get; set; }
+        public V3Dec V { get; set; }
+    }
+
+
+    public HailstoneTwo ParseHailstoneTwo(string s)
+    {
+        var parts = s.Split('@');
+        var p = parts[0].Split(',').Select(a => decimal.Parse(a.Trim())).ToList();
+        var v = parts[1].Split(',').Select(a => decimal.Parse(a.Trim())).ToList();
+
+        return new HailstoneTwo(new V3Dec(p[0], p[1], p[2]), new V3Dec(v[0], v[1], v[2]));
+    }
+
+
 
     public override Answer One(string input)
     {
@@ -89,6 +114,70 @@ public class Day24 : Solution
 
     public override Answer Two(string input)
     {
+        var hailstones = input.Lines().Where(IsNotBlank).Select(ParseHailstoneTwo).ToList();
+
+        var order = hailstones.OrderBy(h => h.P.X).ToList();
+        var diff = order[1].P.X - order[0].P.X;
+
+        var px = hailstones.Min(h => h.P.X);
+
+        var vx = Max(1, hailstones.Max(h => h.V.X));
+
+        // if the rock has same velocity as a hailstone then it must start at the same value
+
+        // rock can't alreay be past a hailstone going in the opposite direction (otherwise t-intersect would be negative)
+        // rock must be on or before hailstone going in same direction
+        // so rock must be before all hailstones
+        // rock speed must be greater than all hailstones going in same direction (can be smaller mag than ones going opposite)
+
+        // so we have a minimum speed, what is the maximum?
+        // too fast if it skips past all stones going in opposite dir?
+
+
+
+        while (!NewMethod(order, px, vx) && vx < 10000)
+        {
+            vx++;
+        }
+        // work out what time the rock will intersect each hailstone
+        /*
+        locRock = Pr + Vr * t
+        locHail = Ph + Vh * t
+        Pr + Vr*t = Ph + Vh*t
+        Pr - Ph = Vh*t - Vr*t
+        Pr - Ph = (Vh - Vr)*t
+        t = (Pr - Ph)/(Vh - Vr)
+
+        t must be positive for each hailstone
+        Assumption t is a whole number for each hailstone
+
+        difference in velocity must be less than difference in position and divide it exactly
+        */
+
         return 0;
+    }
+
+    private static bool NewMethod(List<HailstoneTwo> stones, decimal px, decimal vx)
+    {
+        foreach (var h in stones)
+        {
+            // t = (Pr - Ph)/(Vh - Vr)
+            if (h.V.X != vx)
+            {
+                var deltaP = px - h.P.X;
+                var deltaV = h.V.X - vx;
+                var t = deltaP / deltaV;
+                if (deltaP % deltaV != 0)
+                    return false;
+            }
+            else
+            {
+                // Starting positions mus be the same
+                if (h.P.X != px)
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
