5336995ee7bc0c527f3e0bbcb57bca0831f91a21
Ross McNab <rmcnab@gmail.com>
2024-11-24 16:30:24 +0000

    2020 day 6

---
+++ Solutions/2020/Day06.cs
@@ -0,0 +1,30 @@
+
+namespace AdventOfCode.Year2020;
+
+public class Day06 : Solution
+{
+    public override string FilePath => MetaHelper.FilePath();
+    public override string Example => @"";
+
+    public override Answer One(string input)
+    {
+        return input.Lines().Segment(IsBlank).Sum(CountAnyoneYes);
+    }
+
+    private int CountAnyoneYes(IEnumerable<string> enumerable)
+    {
+        return string.Join("", enumerable.Select(s => s.Trim())).Distinct().Count();
+    }
+
+    public override Answer Two(string input)
+    {
+        return input.Lines().Segment(IsBlank).Sum(CountEveryoneYes);
+    }
+
+    private int CountEveryoneYes(IEnumerable<string> enumerable)
+    {
+        var people = enumerable.Where(IsNotBlank).ToList();
+        var answers = string.Join("", people.Select(s => s.Trim())).Distinct().ToArray();
+        return answers.Count(a => people.All(p => p.Contains(a)));
+    }
+}
