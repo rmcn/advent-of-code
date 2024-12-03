6f91862783d597e57a7cfd0888a727b70a58e616
Ross McNab <rmcnab@gmail.com>
2024-12-03 07:54:22 +0000

    2024 day 3

---
+++ Solutions/2024/Day03.cs
@@ -0,0 +1,32 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2024;
+
+public class Day03 : Solution
+{
+    public override string FilePath => MetaHelper.FilePath();
+    public override string Example => @"";
+
+    public override Answer One(string input)
+    {
+        return Regex.Matches(input, @"mul\(\d+,\d+\)").Sum(m => m.Value.Ints().Product());
+    }
+
+    public override Answer Two(string input)
+    {
+        var t = 0;
+
+        bool include = true;
+        foreach (var m in Regex.Matches(input, @"(mul\(\d+,\d+\)|do\(\)|don't\(\))").ToList())
+        {
+            if (m.Value == "do()")
+                include = true;
+            else if (m.Value == "don't()")
+                include = false;
+            else if (include)
+                t += m.Value.Ints().Product();
+        }
+    
+        return t;
+    }
+}
