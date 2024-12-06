486936b29f3e7f077428624abf441a665954a8ed
Ross McNab <rmcnab@gmail.com>
2024-12-06 19:52:35 +0000

    Filter on command line

--- Core/Commands.cs
+++ Core/Commands.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using AdventOfCode;
 
 namespace AdventOfCode;
 
@@ -9,25 +8,48 @@ public static class Commands
 
     public static int Run(Assembly[] assemblies, string[] args)
     {
-        // Create a new solution file via:
-        //    dotnet run -- new [year day]
-        if (args.Any() &&  args.First() == "new")
-            return MetaHelper.NewDay(args);
+        switch (args.FirstOrDefault())
+        {
+            case "new":
+                // Create a new solution file via:
+                //    dotnet run -- new [year day]
+                return MetaHelper.NewDay(args);
 
+            default:
+                foreach (var solution in Solutions(assemblies, args))
+                {
+                    Advent.Run(solution);
+                }
+                return 0;
+        }
+    }
+
+    private static IEnumerable<Solution> Solutions(Assembly[] assemblies, IEnumerable<string> args)
+    {
         var solutions = assemblies
             .SelectMany(a => a.GetTypes())
             .Where(t => typeof(Solution).IsAssignableFrom(t) && t != typeof(Solution))
-            .Select(t => (Solution)Activator.CreateInstance(t)!)
-            .Where(s => s.Year != 0)
-            .OrderBy(s => s.Year)
-            .ThenBy(s => s.Day)
-            .ToList();
+            .Select(t => (Solution)Activator.CreateInstance(t)!);
 
-        //solutions.ForEach(s => Advent.Run(s));
-        //Advent.Run(solutions.Single(s => s.Year == 2022 && s.Day == 6));
-        //Advent.Run(solutions.Last());
-        Advent.Run(solutions.OrderBy(s => s.LastModified).Last());
-        return 0;
+        var filter = args.ToList();
+
+        if (filter.Count == 0)
+            return new [] { solutions.OrderBy(s => s.Year).ThenBy(s => s.Day).Last() };
+
+        if (filter.FirstOrDefault() == "all")
+            return solutions;
+
+        if (assemblies.Length == 1)
+        {
+            if (filter.Count() != 1 || !int.TryParse(filter[0], out var dayOnly))
+                throw new ArgumentException("Expected day");
+
+            return new [] { solutions.OrderBy(s => s.Year).Where(s => s.Day == dayOnly).Last() };
+        }
+
+        if (filter.Count() != 2 || !int.TryParse(filter[0], out var year) || !int.TryParse(filter[1], out var day))
+            throw new ArgumentException("Expected year day");
+            
+        return new [] { solutions.Where(s => s.Year == year && s.Day == day).Last() };
     }
-
 }
